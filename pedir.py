8a8aea59b4b6cbf1657b798877437a86b9509383
f205771778 <felip.gonzalezn@duocuc.cl>
2023-09-04 23:26:31 +0000

    “Evaluación_N1_DRY7122”

---
+++ pedir.py
@@ -0,0 +1,6 @@
+nombre = input("ingrese su nombre")
+apellido = input("ingrese su apellido")
+seccion = input("ingrese su seccion")
+sede = input("ingrese su sede")
+
+print("hola", nombre, apellido,"de la seccion", seccion,"de la sede", sede)
